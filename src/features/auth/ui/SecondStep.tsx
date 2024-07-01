b7c95211c2973f455f9f2ddf911d42d0e7619b06
Andrey <andreii.rubik@mail.ru>
2024-07-01 17:50:36 +0300

    fix bag

--- src/features/auth/ui/SecondStep.tsx
+++ src/features/auth/ui/SecondStep.tsx
@@ -1,14 +1,11 @@
-import { Input } from "@/shared/components/input/Input";
 import { useState } from "react";
-import { Controller, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
-import style from "./auth.module.scss";
-import { Button } from "@/shared/components/button/Button";
-
+import { Box, Button, IconButton, TextField } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
 interface FormData {
-  email: string;
-  password: string;
+  code: string;
 }
 
 export const SecondStep = () => {
@@ -19,44 +16,84 @@ export const SecondStep = () => {
   const {
     formState: { errors },
     handleSubmit,
-    control,
+    register,
   } = useForm<FormData>();
 
   const onSubmit = () => {
     navigate("/");
   };
 
-  return (
-    <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
-      <Controller
-        name={"email"}
-        control={control}
-        rules={{
-          required: "поле должно быть заполнено",
-          pattern: {
-            message: "Некоректные сиволы",
-            value: /^\S+@\S+$/i,
-          },
-        }}
-        render={({ field }) => (
-          <>
-            <Input
-              placeholder={"email"}
-              type={"text"}
-              name="email"
-              value={email}
-              onChange={(event) => {
-                field.onChange(event);
-                setEmail(event.target.value);
-              }}
-            />
-          </>
-        )}
-      />
-      {errors.email?.message}
+  function resetField(arg0: string) {
+    throw new Error("Function not implemented.");
+  }
 
-      {errors.password?.message}
-      <Button children={"Войти"} type={"submit"} />
-    </form>
+  return (
+    <Box
+      component={"form"}
+      onSubmit={handleSubmit(onSubmit)}
+      sx={{
+        width: "100%",
+        marginBottom: "3rem",
+        padding: "1rem",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "1.5rem",
+      }}
+    >
+      <TextField
+        sx={{ width: "100%" }}
+        label={"code*"}
+        {...register("code")}
+        error={!!errors.code}
+        helperText={errors.code?.message}
+        InputProps={{
+          endAdornment: (
+            <IconButton
+              onClick={() => {
+                resetField("code");
+              }}
+            >
+              <ClearIcon />
+            </IconButton>
+          ),
+        }}
+      />
+      <Button sx={{ width: "100%" }} variant="contained" type="submit">
+        Войти
+      </Button>
+    </Box>
+    // <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
+    //   <Controller
+    //     name={"email"}
+    //     control={control}
+    //     rules={{
+    //       required: "поле должно быть заполнено",
+    //       pattern: {
+    //         message: "Некоректные сиволы",
+    //         value: /^\S+@\S+$/i,
+    //       },
+    //     }}
+    //     render={({ field }) => (
+    //       <>
+    //         <Input
+    //           placeholder={"email"}
+    //           type={"text"}
+    //           name="email"
+    //           value={email}
+    //           onChange={(event) => {
+    //             field.onChange(event);
+    //             setEmail(event.target.value);
+    //           }}
+    //         />
+    //       </>
+    //     )}
+    //   />
+    //   {errors.email?.message}
+
+    //   {errors.password?.message}
+    //   <Button children={"Войти"} type={"submit"} />
+    // </form>
   );
 };
