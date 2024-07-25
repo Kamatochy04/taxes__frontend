b9a7d9064b18f1146039a0e99e9d67f1c1f95f48
Сергей Шевцов <asuranserg88@gmail.com>
2024-07-25 19:38:50 +0300

    стили для "способы оплаты"

--- src/widgets/paymentMethods/ui/PaymentMethods.tsx
+++ src/widgets/paymentMethods/ui/PaymentMethods.tsx
@@ -5,8 +5,8 @@ export const PaymentMethods = () => {
       <div className={style.card}>
         <div className={style.card__img}></div>
         <div className={style.card__wrapper}>
-          <p>Карта 1234 5678 9123 8526</p>
-          <p>09/28</p>
+          <p className={style.text}>Карта 1234 5678 9123 8526</p>
+          <p className={style.text}>09/28</p>
         </div>
       </div>
     </div>
