af7315da7ae3e6ec3827a72667d0b03dfc5b2489
Сергей Шевцов <asuranserg88@gmail.com>
2024-07-25 20:12:23 +0300

    стили для "моя корзина"

--- src/widgets/basket/ui/Basket.tsx
+++ src/widgets/basket/ui/Basket.tsx
@@ -3,11 +3,21 @@ import style from "./basket.module.scss";
 
 export const Basket = () => {
   return (
-    <section className={style.cards}>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </section>
+    <>
+      <section className={style.cards}>
+        <Card />
+        <Card />
+        <Card />
+      </section>
+
+      <div className={style.blockButton}>
+        <button className={style.clear}>
+          Очистить
+        </button>
+        <button className={style.order}>
+          Заказать
+        </button>
+      </div>
+    </>
   );
 };
