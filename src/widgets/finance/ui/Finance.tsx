0ebeae986ab034024e4c8bd0b6ab3834b65e5dcb
Сергей Шевцов <asuranserg88@gmail.com>
2024-07-29 20:02:06 +0300

    страница "мои финансы"

--- src/widgets/finance/ui/Finance.tsx
+++ src/widgets/finance/ui/Finance.tsx
@@ -1,40 +1,65 @@
-// import { useRef } from "react";
-// import style from "./finance.module.scss";
+import { Path } from "@/widgets";
+import style from "./finance.module.scss";
+import { useState } from "react";
 
-// export const Finance = () => {
-//   const ref = useRef();
-//   return (
-//     <form
-//       oninput={`${(result.value = parseInt(revenue.value) / 100)}*${parseInt(
-//         percent.value
-//       )}`}
-//     >
-//       <div className={style.finance}>
-//         <div className={style.card}>
-//           <p className={style.finance__text}>Процентная ставка, %</p>
-//           <div className={style.field}>
-//             <input type="number"  name="percent" min="1" max="100" value="10" />
-//             <p className={style.finance__text}>%</p>
-//           </div>
-//         </div>
-//         <div className={style.card}>
-//           <p className={style.finance__text}>Выручка за месяц составляет</p>
-//           <div className={style.field}>
-//             <input type="number" name="revenue" value="1400" />
-//             <p className={style.finance__text}>BYN</p>
-//           </div>
-//         </div>
-//         <div className={style.card}>
-//           <p className={style.finance__text}>Налог за месяц составляет</p>
-//           <div className={style.field}>
-//             <output name="result">140</output>
-//             <p className={style.finance__text}>BYN</p>
-//           </div>
-//         </div>
-//       </div>
-//       <button className={style.button} type="submit">
-//         Оплатить
-//       </button>
-//     </form>
-//   );
-// };
+export const Finance = () => {
+  const [percent, setPercent] = useState<string>("");
+  const [revenue, setRevenue] = useState<string>("");
+
+  const handlePercentEvent = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPercent(e.target.value);
+  };
+
+  const handleRevenueEvent = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRevenue(e.target.value);
+  };
+
+  return (
+    <>
+      <Path />
+      <div className={style.finance}>
+
+        <div className={style.card}>
+
+          <div className={style.input}>
+            <p className={style.finance__text}>Процентная ставка, %</p>
+            <div className={style.field}>
+              <input
+                value={percent}
+                onChange={handlePercentEvent}
+                type="text"
+                name="percent"
+                min="1"
+                max="100"
+              />
+              <p className={style.finance__text}>%</p>
+            </div>
+          </div>
+          <div className={style.input}>
+            <p className={style.finance__text}>Выручка за месяц составляет</p>
+            <div className={style.field}>
+              <input
+                value={revenue}
+                onChange={handleRevenueEvent}
+                type="text"
+                name="revenue"
+              />
+              <p className={style.finance__text}>BYN</p>
+            </div>
+          </div>
+          <div className={style.input}>
+            <p className={style.finance__text}>Налог за месяц составляет</p>
+            <div className={style.field}>
+              <span>{((parseInt(revenue) / 100) * parseInt(percent)).toFixed(2)}</span>
+              <p className={style.finance__text}>BYN</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <button className={style.button} type="submit">
+        Оплатить
+      </button>
+    </>
+  );
+};
