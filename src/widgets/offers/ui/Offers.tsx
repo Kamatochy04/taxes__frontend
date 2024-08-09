f77ad9130ee4ce10d5bca020b31fee20054c4d54
Сергей Шевцов <asuranserg88@gmail.com>
2024-08-09 09:31:38 +0300

    начало верстки offers

--- src/widgets/offers/ui/Offers.tsx
+++ src/widgets/offers/ui/Offers.tsx
@@ -1,98 +1,20 @@
-import { DefaultImg } from "@/shared/assets/icons/DefaultImg";
 import style from "./offers.module.scss";
-import { Path } from "@/widgets";
+import { CardBasket, Path, CardAddPost } from "@/widgets";
 
 export const Offers = () => {
+
   return (
     <>
       <Path />
-      <div className={style.offers}>
-        <div className={style.card}>
-          <div className={style.card__img}>
-            <DefaultImg />
-          </div>
-          <p>Дата: 09.02.2024</p>
-          <p>Подробнее</p>
-        </div>
-        <div className={style.card}>
-          <div className={style.card__img}>
-            <DefaultImg />
-          </div>
-          <p>Дата: 09.02.2024</p>
-          <p>Подробнее</p>
-        </div>
-        <div className={style.card}>
-          <div className={style.card__img}>
-            <DefaultImg />
-          </div>
-          <p>Дата: 09.02.2024</p>
-          <p>Подробнее</p>
-        </div>
-        <div className={style.card}>
-          <div className={style.card__img}>
-            <DefaultImg />
-          </div>
-          <p>Дата: 09.02.2024</p>
-          <p>Подробнее</p>
-        </div>
-
-        <div className={style.card}>
-          <div className={style.card__img}>
-            <DefaultImg />
-          </div>
-          <p>Дата: 09.02.2024</p>
-          <p>Подробнее</p>
-        </div>
-        <div className={style.card}>
-          <div className={style.card__img}>
-            <DefaultImg />
-          </div>
-          <p>Дата: 09.02.2024</p>
-          <p>Подробнее</p>
-        </div>
-        <div className={style.card}>
-          <div className={style.card__img}>
-            <DefaultImg />
-          </div>
-          <p>Дата: 09.02.2024</p>
-          <p>Подробнее</p>
-        </div>
-        <div className={style.card}>
-          <div className={style.card__img}>
-            <DefaultImg />
-          </div>
-          <p>Дата: 09.02.2024</p>
-          <p>Подробнее</p>
-        </div>
-        <div className={style.card}>
-          <div className={style.card__img}>
-            <DefaultImg />
-          </div>
-          <p>Дата: 09.02.2024</p>
-          <p>Подробнее</p>
-        </div>
-        <div className={style.card}>
-          <div className={style.card__img}>
-            <DefaultImg />
-          </div>
-          <p>Дата: 09.02.2024</p>
-          <p>Подробнее</p>
-        </div>
-        <div className={style.card}>
-          <div className={style.card__img}>
-            <DefaultImg />
-          </div>
-          <p>Дата: 09.02.2024</p>
-          <p>Подробнее</p>
-        </div>
-        <div className={style.card}>
-          <div className={style.card__img}>
-            <DefaultImg />
-          </div>
-          <p>Дата: 09.02.2024</p>
-          <p>Подробнее</p>
-        </div>
+      <div className={style.fild}>
+        <section className={style.cards}>
+          <CardBasket />
+          <CardBasket />
+          <CardBasket />
+        </section>
+        <CardAddPost text={"Добавить предложение"} variant={"addSentence"}/>
       </div>
+
     </>
   );
 };
