f3a6a16607c21b5a893585443d2f20e463fbe365
Сергей Шевцов <asuranserg88@gmail.com>
2024-09-19 19:25:47 +0300

    +

--- src/widgets/orders/ui/Orders.tsx
+++ src/widgets/orders/ui/Orders.tsx
@@ -1,4 +1,4 @@
-import { DefaultImg } from "@/shared/assets/icons/DefaultImg";
+//import { DefaultImg } from "@/shared/assets/icons/DefaultImg";
 import style from "./orders.module.scss";
 import { Path } from "@/widgets";
 import { useGetUserMeOrdersQuery } from "@/features/user/api/AccountApi";
@@ -21,93 +21,39 @@ export const Orders = () => {
             ))
           : "ERROR"}
 
-        <div className={style.card}>
-          <div className={style.card__title}>
-            <h2>Заказ №102558</h2>
-            <input className={style.card__input} type="checkbox" />
-          </div>
-
-          <div className={style.card__field}>
-            <div className={style.card__img}>
-              <DefaultImg />
-            </div>
-
-            <div className={style.card__descr}>
-              <p className={style.card__fieldName}>Покупатель</p>
-              <p className={style.card__fieldName}>Контактные данные</p>
-              <p className={style.card__fieldName}>Условия доставки</p>
-              <p className={style.card__fieldName}>Условия оплаты</p>
-            </div>
-
-            <div className={style.card__descr}>
-              <p className={style.card__fieldValues}>Петрова Мария</p>
-              <p className={style.card__fieldValues}>Контактные данные</p>
-              <p className={style.card__fieldValues}>
-                пункт выдачи №56, г. Минск, ул. Щорса, 17
-              </p>
-              <p className={style.card__fieldValues}>при получении</p>
-            </div>
-          </div>
-        </div>
-
-        <div className={style.card}>
-          <div className={style.card__title}>
-            <h2>Заказ №102558</h2>
-            <input className={style.card__input} type="checkbox" />
-          </div>
-
-          <div className={style.card__field}>
-            <div className={style.card__img}>
-              <DefaultImg />
-            </div>
-
-            <div className={style.card__descr}>
-              <p className={style.card__fieldName}>Покупатель</p>
-              <p className={style.card__fieldName}>Контактные данные</p>
-              <p className={style.card__fieldName}>Условия доставки</p>
-              <p className={style.card__fieldName}>Условия оплаты</p>
-            </div>
-
-            <div className={style.card__descr}>
-              <p className={style.card__fieldValues}>Петрова Мария</p>
-              <p className={style.card__fieldValues}>Контактные данные</p>
-              <p className={style.card__fieldValues}>
-                пункт выдачи №56, г. Минск, ул. Щорса, 17
-              </p>
-              <p className={style.card__fieldValues}>при получении</p>
-            </div>
-          </div>
-        </div>
-
-        <div className={style.card}>
-          <div className={style.card__title}>
-            <h2>Заказ №102558</h2>
-            <input className={style.card__input} type="checkbox" />
-          </div>
-
-          <div className={style.card__field}>
-            <div className={style.card__img}>
-              <DefaultImg />
-            </div>
-
-            <div className={style.card__descr}>
-              <p className={style.card__fieldName}>Покупатель</p>
-              <p className={style.card__fieldName}>Контактные данные</p>
-              <p className={style.card__fieldName}>Условия доставки</p>
-              <p className={style.card__fieldName}>Условия оплаты</p>
-            </div>
-
-            <div className={style.card__descr}>
-              <p className={style.card__fieldValues}>Петрова Мария</p>
-              <p className={style.card__fieldValues}>Контактные данные</p>
-              <p className={style.card__fieldValues}>
-                пункт выдачи №56, г. Минск, ул. Щорса, 17
-              </p>
-              <p className={style.card__fieldValues}>при получении</p>
-            </div>
-          </div>
-        </div>
       </div>
     </>
   );
 };
+
+
+/*
+        <div className={style.card}>
+          <div className={style.card__title}>
+            <h2>Заказ №102558</h2>
+            <input className={style.card__input} type="checkbox" />
+          </div>
+
+          <div className={style.card__field}>
+            <div className={style.card__img}>
+              <DefaultImg />
+            </div>
+
+            <div className={style.card__descr}>
+              <p className={style.card__fieldName}>Покупатель</p>
+              <p className={style.card__fieldName}>Контактные данные</p>
+              <p className={style.card__fieldName}>Условия доставки</p>
+              <p className={style.card__fieldName}>Условия оплаты</p>
+            </div>
+
+            <div className={style.card__descr}>
+              <p className={style.card__fieldValues}>Петрова Мария</p>
+              <p className={style.card__fieldValues}>Контактные данные</p>
+              <p className={style.card__fieldValues}>
+                пункт выдачи №56, г. Минск, ул. Щорса, 17
+              </p>
+              <p className={style.card__fieldValues}>при получении</p>
+            </div>
+          </div>
+        </div>
+        */
