6dc25a627c2d928fd9ad9b6fdd3c81586ce19af8
Сергей Шевцов <asuranserg88@gmail.com>
2024-09-23 14:21:02 +0300

    выровнял input

--- src/widgets/account/ui/PassportDetails.tsx
+++ src/widgets/account/ui/PassportDetails.tsx
@@ -11,20 +11,22 @@ interface PersonalDetaProps {
   data: AccountData;
 }
 
-export const PassportDetails: FC<PersonalDetaProps> = ({ data }) =>{
-
+export const PassportDetails: FC<PersonalDetaProps> = ({ data }) => {
   const [addPassport] = useState<AccountData>({
-    id: `${data.id !== null ? data.id : ''}`,
-    email: `${data.email !== null ? data.email : ''}`,
-    first_name: `${data.first_name !==null ? data.first_name : ''}`,
-    last_name: `${data.last_name !== null ? data.last_name : ''}`,
-    patronymic: `${data.patronymic !== null ? data.patronymic : ''}`,
+    id: `${data.id !== null ? data.id : ""}`,
+    email: `${data.email !== null ? data.email : ""}`,
+    first_name: `${data.first_name !== null ? data.first_name : ""}`,
+    last_name: `${data.last_name !== null ? data.last_name : ""}`,
+    patronymic: `${data.patronymic !== null ? data.patronymic : ""}`,
 
     //unp: `${data.unp !== null ? data.unp : ''}`,
-    registration_address: `${data.registration_address !== null ? data.registration_address : ''}`,
-    //residential_address: `${data.residential_address !== null ? data.residential_address : ''}`,    
-    date_of_birth: data.date_of_birth !== null ? data.date_of_birth : new Date,
-    passport_num: `${data.passport_num !== null ? data.passport_num : ''}`,
+    registration_address: `${
+      data.registration_address !== null ? data.registration_address : ""
+    }`,
+    //residential_address: `${data.residential_address !== null ? data.residential_address : ''}`,
+    date_of_birth:
+      data.date_of_birth !== null ? data.date_of_birth : new Date(),
+    passport_num: `${data.passport_num !== null ? data.passport_num : ""}`,
     //phone_number: `${data.phone_number !== null ? data.phone_number : ''}`,
     //date_of_birth: '2024-09-11',
   });
@@ -42,17 +44,9 @@ export const PassportDetails: FC<PersonalDetaProps> = ({ data }) =>{
       >
         {({ isValid }) => (
           <Form>
-            <div className={style.card__Line}>
-              <div className={style.card__Column}>
-                <Typography variant="default" tag={"p"}>
-                  Дата рождения
-                </Typography>
-                <Typography variant="default" tag={"p"}>
-                  Паспорт
-                </Typography>
-              </div>
-
-              <div className={style.card__Column}>
+            <div className={style.card__ColumnX}>
+              <div className={style.card__LineX}>
+                <p className={style.card__text}>Дата рождения</p>
                 <Field
                   className={style.card__input}
                   name={"date_of_birth"}
@@ -60,36 +54,38 @@ export const PassportDetails: FC<PersonalDetaProps> = ({ data }) =>{
                   autoComplete="off"
                   placeholder="дата"
                 />
-                <div className={style.card__Line}>
-                  <Field
-                    className={style.card__input}
-                    name={"passport_num"}
-                    type="text"
-                    autoComplete="off"
-                    placeholder="серия и номер"
-                  />
-                </div>
-                <div className={style.card__Line}>
-                  <Button type="submit" variant={"text"} disabled={!isValid}>
-                    <Typography variant="default" tag={"p"}>
-                      Изменить
-                    </Typography>
-                  </Button>
-                </div>
               </div>
-            </div>
-            <div className={style.card__Line}>
-              <Typography variant="default" tag={"p"}>
-                Адрес регистрации
-              </Typography>
 
-              <Field
-                className={style.card__input}
-                name={"registration_address"}
-                type="text"
-                autoComplete="off"
-                placeholder="регистрация"
-              />
+              <div className={style.card__LineX}>
+                <p className={style.card__text}>Паспорт</p>
+                <Field
+                  className={style.card__input}
+                  name={"passport_num"}
+                  type="text"
+                  autoComplete="off"
+                  placeholder="серия и номер"
+                />
+              </div>
+
+              <div className={style.card__LineX}>
+                <p className={style.card__text}>Адрес регистрации</p>
+                <Field
+                  className={style.card__input}
+                  name={"registration_address"}
+                  type="text"
+                  autoComplete="off"
+                  placeholder="регистрация"
+                />
+              </div>
+
+              <div className={style.card__LineX}>
+                <p className={style.card__text}></p>
+                <Button type="submit" variant={"text"} disabled={!isValid}>
+                  <Typography variant="default" tag={"p"}>
+                    Изменить
+                  </Typography>
+                </Button>
+              </div>
             </div>
           </Form>
         )}
--- src/widgets/account/ui/TaxpayerDetails.tsx
+++ src/widgets/account/ui/TaxpayerDetails.tsx
@@ -17,23 +17,22 @@ interface PersonalDetaProps {
 }
 
 export const TaxpayerDetails: FC<PersonalDetaProps> = ({ dataUser }) => {
-  
   const { data } = useGetCategoriesQuery("");
 
   const text = `Информация по добавлению инфы
   - символы
   - количество`;
-  
-  const [addTaxpayer] = useState<AccountData>({
-    id: `${dataUser.id !== null ? dataUser.id : ''}`,
-    email: `${dataUser.email !== null ? dataUser.email : ''}`,
-    first_name: `${dataUser.first_name !==null ? dataUser.first_name : ''}`,
-    last_name: `${dataUser.last_name !== null ? dataUser.last_name : ''}`,
-    patronymic: `${dataUser.patronymic !== null ? dataUser.patronymic : ''}`,
 
-    unp: `${dataUser.unp !== null ? dataUser.unp : ''}`,
+  const [addTaxpayer] = useState<AccountData>({
+    id: `${dataUser.id !== null ? dataUser.id : ""}`,
+    email: `${dataUser.email !== null ? dataUser.email : ""}`,
+    first_name: `${dataUser.first_name !== null ? dataUser.first_name : ""}`,
+    last_name: `${dataUser.last_name !== null ? dataUser.last_name : ""}`,
+    patronymic: `${dataUser.patronymic !== null ? dataUser.patronymic : ""}`,
+
+    unp: `${dataUser.unp !== null ? dataUser.unp : ""}`,
     //registration_address: `${dataUser.registration_address !== null ? dataUser.registration_address : ''}`,
-    //residential_address: `${dataUser.residential_address !== null ? dataUser.residential_address : ''}`,    
+    //residential_address: `${dataUser.residential_address !== null ? dataUser.residential_address : ''}`,
     //date_of_birth: dataUser.date_of_birth !== null ? dataUser.date_of_birth : new Date,
     //passport_num: `${dataUser.passport_num !== null ? dataUser.passport_num : ''}`,
     //phone_number: `${dataUser.phone_number !== null ? dataUser.phone_number : ''}`,
@@ -51,49 +50,44 @@ export const TaxpayerDetails: FC<PersonalDetaProps> = ({ dataUser }) => {
       >
         {({ isValid }) => (
           <Form>
-            <div className={style.card__Line}>
-              <div className={style.card__Column}>
-                <Typography variant="default" tag={"p"}>
-                  УНП
-                </Typography>
-                <Typography variant="default" tag={"p"}>
-                  Категория услуг
-                </Typography>
-              </div>
-
-              <div className={style.card__Column}>
-                <HintCloud text={text}>
-                  <Field
-                    className={style.card__input}
-                    autoComplete="off"
-                    name={"unp"}
-                    type="text"
-                    placeholder="Введите УНП"
-                  />
-                </HintCloud>
+            <div className={style.card__ColumnX}>
+              <div className={style.card__LineX}>
+                <p className={style.card__text}>УНП</p>
 
                 <Field
                   className={style.card__input}
-                  name="сategory"
+                  autoComplete="off"
+                  name={"unp"}
                   type="text"
-                  placeholder="Введите категорию товара"
+                  placeholder="Введите УНП"
+                />
+              </div>
+
+              <div className={style.card__LineX}>
+                <p className={style.card__text}>Категория услуг</p>
+                <Field
+                  className={style.card__input}
+                  name="сategory"
+                  placeholder="категорию"
                   as="select"
                 >
-                    {data != undefined
-                      ? data.results.map((item: CategoriesType) => (
-                          <option value={item.id} key={item.id}>
-                            {item.name}
-                          </option>
-                        ))
-                      : "ERROR"}
+                  {data != undefined
+                    ? data.results.map((item: CategoriesType) => (
+                        <option value={item.id} key={item.id}>
+                          {item.name}
+                        </option>
+                      ))
+                    : "ERROR"}
                 </Field>
-                <div className={style.card__Line}>
-                  <Button type="submit" variant={"text"} disabled={!isValid}>
-                    <Typography variant="default" tag={"p"}>
-                      Изменить
-                    </Typography>
-                  </Button>
-                </div>
+              </div>
+
+              <div className={style.card__LineX}>
+                <p className={style.card__text}></p>
+                <Button type="submit" variant={"text"} disabled={!isValid}>
+                  <Typography variant="default" tag={"p"}>
+                    Изменить
+                  </Typography>
+                </Button>
               </div>
             </div>
           </Form>
